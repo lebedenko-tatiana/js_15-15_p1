7df5dce3b757ce34e2f5682deb2d8601bab1ff48
Lebedenko Tatiana <lebedenko_tatiana@mail.ru>
2016-09-15 13:19:51 +0300

    second commit

--- js/sqript.js
+++ js/sqript.js
@@ -1,7 +1,47 @@
-﻿function callbackFunc(data) {
+﻿
+$(function () {
+
+  var $modal, $parent, $elem;
+  var $body = $('body');
+
+
+  function callAPI(inputValue) {
+    $.ajax({
+      url:
+      'https://pixabay.com/api/?key=3270070-e448714d3a5efe7670c473a3a&q=' + inputValue + '&image_type=photo&callback=callbackFunc&context=?',
+      dataType: 'jsonp'
+    });
+  }
+
+
+  var $input = $('.search-input');
+  
+  $input.focus();
+  $input.on('keypress', function(e) {
+  		  	   if (e.keyCode == 13) {
+			     e.preventDefault();
+                             $('.wrapper').empty();
+                    	     callAPI($input.val());
+		  	   };
+  });
+
+
+  var $submit = $('.search-submit');
+  $submit.on('click', function(e) {
+		        e.preventDefault();
+                        $('.wrapper').empty();
+			callAPI($input.val());
+  });
+
+ 
+  
+
+});
+
+
+function callbackFunc(data) {
 
   $.each(data.hits, function(i, hit) {
-
                       $parent = $('.wrapper');
 	              $elem = $('<a class="link-item" href="' + hit.webformatURL + '"></a>');
                       $parent.append($elem);   
@@ -9,17 +49,12 @@
                       $parent = $('.link-item');
                       $elem = $('<img class="img-item" src="' + hit.previewURL + '" alt="' + hit.tags + '" >');
                       $parent.last().append($elem);   
-
-
   	            });
-
-
 
   var $link = $('.link-item');
 
   $link.on('mouseenter', showModal)
        .on('mouseleave', hideModal);
-
 
 }
 
@@ -31,8 +66,8 @@ function showModal() {
   var windowWidth = $(window).width();
 
   var href = $(this).attr('href');
-  var leftOffset = $(this)[0].offsetWidth + 5;
-  var topOffset = $(this)[0].offsetHeight;
+  var leftOffset = $(this)[0].offsetWidth + 10;
+  var topOffset = $(this)[0].firstChild.naturalHeight;
 
   $modal = $('<div class="fancybox-modal"><img src="' + href + '"></div>');
   $body.append($modal);
@@ -44,13 +79,13 @@ function showModal() {
 
   var $img = $modal.find('img');
   $img.css('width', leftOffset * 2.5);
-  var imgWidth = $img[0].width + 5; 
+  var imgWidth = $img[0].width + 30; 
 
   if ((offsetPreview.left  + leftOffset + imgWidth) < windowWidth) {
-    $modal.css('top', offsetPreview.top + topOffset).css('left', offsetPreview.left + leftOffset); 
+    $modal.css('top', offsetPreview.top - topOffset).css('left', offsetPreview.left + leftOffset); 
   }
   else {
-    $modal.css('top', offsetPreview.top + topOffset).css('left', offsetPreview.left - imgWidth); 
+    $modal.css('top', offsetPreview.top - topOffset).css('left', offsetPreview.left - imgWidth); 
   }
 }
 
@@ -59,43 +94,3 @@ function showModal() {
 function hideModal() {
   $modal.remove();
 }
-
-
-
-function callAPI(inputValue) {
-  $.ajax({
-    url:
-    'https://pixabay.com/api/?key=3270070-e448714d3a5efe7670c473a3a&q=' + inputValue + '&image_type=photo&callback=callbackFunc&context=?',
-    dataType: 'jsonp'
-  });
-}
-
-
-
-$(function () {
-
-  var $modal, $parent, $elem;
-  var $body = $('body');
-
-
-  var $input = $('.search-input');
-
-  
-/*  $input.keyup(function(e) {
-  		  if (e.keyCode == 13) {
-                    callAPI('леопард');
-		  };
-  });*/
-
-
-  var $submit = $('.search-submit');
-
-  $submit.on('click', callAPI('леопард'));
-
-
-  
-
-
-});
-
-
